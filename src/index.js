973b33161f68eed609f158fd94ab3749991e739e
Chris Pooley <cpooley@gmail.com>
2021-05-17 20:22:31 -0700

    updates

--- src/index.js
+++ src/index.js
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from 'firebase';
+import 'firebase/analytics';
+import 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCCmuzSABDPvMmxDTQMUU0oVl4-uAHnox0",
+  authDomain: "photo-sharing-app-ed43e.firebaseapp.com",
+  projectId: "photo-sharing-app-ed43e",
+  storageBucket: "photo-sharing-app-ed43e.appspot.com",
+  messagingSenderId: "968176746039",
+  appId: "1:968176746039:web:8f1946c0093f19556d49b2",
+  measurementId: "G-K1QXS9M25N"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 ReactDOM.render(
   <React.StrictMode>
